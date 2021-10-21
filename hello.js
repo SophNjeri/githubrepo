2e09da447924f05c30414e9481dc1b328a1304d9
unknown <njerisophia197@gmail.com>
2021-10-21 19:58:04 +0300

    changes done on hello.js

---
+++ hello.js
@@ -0,0 +1,13 @@
+console.log("sophia njeri kogi")
+
+
+const App = () => {
+    const [count, setCount] = useState("")
+    return (
+        <div className="container">
+            <p className="text">
+                Hi there
+            </p>
+        </div>
+    )
+}
---
+++ test.py
@@ -0,0 +1,7 @@
+print("Hello Python")
+
+
+l=[1,2,3,4]
+print(l)
+l.append("a")
+print(l)
